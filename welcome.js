0cc440ab846e6dc2b64d185df2ee72c9e76c26e7
Jaydip Changani <98078979+jaydipchangani@users.noreply.github.com>
2023-07-14 17:26:01 +0530

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <div>
+        <h1>Welcome Aboard !!</h1>
+        <p> I welcome you to my Node.js repo </p>
+    </div>,
+    document.getElementById('root')   );
