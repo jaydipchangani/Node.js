8bff8f6b4075cc6aa00a80b5f4ad8fdc67785f7f
Jaydip Changani <98078979+jaydipchangani@users.noreply.github.com>
2023-07-14 17:47:53 +0530

    Welcome.js

--- welcome.js
+++ welcome.js
@@ -20,3 +20,23 @@ ReactDom.render(
         <p> I welcome you to my Node.js repo </p>
     </React.Fragment>,
     document.getElementById('root')   );
+
+########################################################
+
+import React from 'react';
+import reactDom from 'react-dom';
+
+reactDom.render(
+    <>
+        <h1>My all time Favourite Series Pick</h1>
+        <p>List of 5 Best Series</p>
+        <ol>
+            <li>Asur</li>
+            <li>Kota Factory</li>
+            <li>Scorpion</li>
+            <li>Mr Robot</li>
+            <li>TVF Pitchers</li>
+        </ol>
+    </>,document.getElementById('root'));
+
+
