6a26edf3b881b86d17b42b3989de0e7bf01743df
Jaydip Changani <98078979+jaydipchangani@users.noreply.github.com>
2023-07-14 17:36:08 +0530

    Welcome.js

--- welcome.js
+++ welcome.js
@@ -7,3 +7,16 @@ ReactDom.render(
         <p> I welcome you to my Node.js repo </p>
     </div>,
     document.getElementById('root')   );
+
+#######################################################
+
+//To remove extra div we use <react.Fragment> or just<>
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <React.Fragment>
+        <h1>Welcome Aboard !!</h1>
+        <p> I welcome you to my Node.js repo </p>
+    </React.Fragment>,
+    document.getElementById('root')   );
