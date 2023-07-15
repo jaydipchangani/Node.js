1b4ccf71767899b49892dbe4bd919f34107e795c
Jaydip Changani <98078979+jaydipchangani@users.noreply.github.com>
2023-07-15 10:19:06 +0530

    Sample_Galllery.js

--- Sample_Galllery.js
+++ Sample_Galllery.js
@@ -63,3 +63,41 @@ CSS Code
   height: 300px;
 
 }
+
+
+#########################################################################
+Inline CSS in React
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css'
+
+const img1="https://picsum.photos/200/300"
+const img2="https://picsum.photos/250/300"
+const img4="https://picsum.photos/210/300"
+const heading ={
+    color:"rgb(25, 120, 245)",
+    margin :"20px",
+    textAlign:"Center",
+    textTransform : "capitalize" ,
+    fontFamily : "Poppins",
+    fontWeight: "bold"
+}
+
+ReactDom.render(<>
+    <h1 style={heading}>Welcome to Gallery</h1>
+    <div className='imgdiv'>
+        <img src={img1} alt='Image1' className='img' /> 
+        <img src={img2} alt='Image1' className='img' />
+        <img src={img4} alt='Image1' className='img' /> 
+       
+    </div>
+
+    <div className='img2div'>
+        <img src={img1} alt='Image1' className='img' /> 
+        <img src={img2} alt='Image1' className='img' />
+        <img src={img4} alt='Image1' className='img' /> 
+       
+    </div>
+    </>
+    ,document.getElementById('root'))
